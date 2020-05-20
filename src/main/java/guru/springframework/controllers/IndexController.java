67b884d0c49089054d9280ee6db26ae690136345
Seth Wheeler <wheeler.seth@gmail.com>
2020-05-20 14:35:18 -0600

    cloning update repo for course

---
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -0,0 +1,40 @@
+package guru.springframework.controllers;
+
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+/**
+ * Created by jt on 6/1/17.
+ */
+@Controller
+public class IndexController {
+
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(){
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Category ID is: " + categoryOptional.get().getId());
+        System.out.println("UOM ID is: " + unitOfMeasure.get().getId());
+
+
+
+        return "index";
+    }
+}
